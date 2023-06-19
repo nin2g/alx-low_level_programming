e7652241d6606881ddbc409c66162a31081dff17
nin2g <nin2g@yahoo.com>
2023-06-19 16:50:08 -0700

    pralp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lower case except q and e.
+ * Return: should return 0.
+ */
+
+int main(void)
+{
+	char lc, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+	if (lc != e && lc != q)
+	putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}	
