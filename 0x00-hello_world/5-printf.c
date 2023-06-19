dde7fa4c8dc93eed734f2b1ef962a1bc8c42a8d8
nin2g <nin2g@yahoo.com>
2023-06-18 18:31:32 -0700

    prnf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /** 
  * main - prints "with proper grammar, but the outcome is a piece of art,.
@@ -11,4 +11,5 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 
-}	
+}
+
