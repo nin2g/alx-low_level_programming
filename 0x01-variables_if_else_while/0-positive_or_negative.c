1a42387168bb1df39e297da2364f1dbc49427ade
nin2g <nin2g@yahoo.com>
2023-06-19 00:36:16 -0700

    pstneg

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints whether the random number is positive, negative or zero.
+ * Return: should return 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	=rand() - RAND_MAZ /2;
+	if (n>0)
+		printf("is positive");
+	else if (n==0)
+		printf("is zero");
+	else
+		printf("is negaive");
+	return (0);
+}
