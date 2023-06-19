05a7c807395d888db2858e775593dfe57af3fd86
nin2g <nin2g@yahoo.com>
2023-06-18 18:20:21 -0700

    puttin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
- * Return - Always 0.
+ * main - prints "Programming is like building a multilingual puzzle.
+ * Return: should return 0.
  */
 
 int main(void)
