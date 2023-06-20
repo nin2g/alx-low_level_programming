8f35d79bdcef8a3f2494ef83294ec991b1e4502d
nin2g <nin2g@yahoo.com>
2023-06-20 15:15:16 -0700

    alfab

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * main - print alphabet in lower case.
+ * Return: should return 0.
+ */
+
+int main(void)
+
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+}
