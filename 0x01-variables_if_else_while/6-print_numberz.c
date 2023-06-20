77e7bd736c51d15947b27632ad41a7e7c3801e0f
nin2g <nin2g@yahoo.com>
2023-06-19 17:12:36 -0700

    prnumz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0.
+ * Return: should return 0.
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}	
