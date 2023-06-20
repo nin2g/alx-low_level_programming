c8454c22788a3639081e2828d408c41a448294bd
nin2g <nin2g@yahoo.com>
2023-06-20 16:08:34 -0700

    alphat

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - display aplhabet 10 times.
+ * Return: returns void.
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char b;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (b = 'a'; b <= 'z'; b++)
+	{
+	_putchar(b);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
