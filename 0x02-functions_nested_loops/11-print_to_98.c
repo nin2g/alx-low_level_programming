dc02a7042bfd0840f939065010b6bed5220379b3
nin2g <nin2g@yahoo.com>
2023-06-21 13:33:59 -0700

    rnt98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers in sequence from 0 to 98.
+ * @n: the initial number.
+ * Return: should return 0.
+ */
+
+void print_to_98(int n)
+
+{
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+	if (n == 98)
+	{
+	printf("d", n);
+	printf("/n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
+}
+
