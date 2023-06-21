94c2d02eff774c0905e68a9b69afd53264148e52
nin2g <nin2g@yahoo.com>
2023-06-20 17:48:45 -0700

    isalp

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <"main">
+/**
+ * _isalpha - checks for alphabetic character.
+ * Returns: should return 1 for  alphabetic character otherwise 0.
+ * @c: the input character to be checked.
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
