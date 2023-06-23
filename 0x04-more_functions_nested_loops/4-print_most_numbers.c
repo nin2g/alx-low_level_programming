b8eeb2a19dc98e670e3bbd2f648b531a5a24453d
nin2g <nin2g@yahoo.com>
2023-06-22 19:07:08 -0700

    prntnum

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * Return: should return numbers from 0 to 9.
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (; a <= 0; a++)
+	{
+	if (a == 2 || a == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(a + '0');
+	}
+	}
+	_putchar('\n')
+}
