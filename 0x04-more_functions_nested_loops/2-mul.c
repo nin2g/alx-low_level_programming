d0af1c2321e82a65422af4064b6b6f4d672ec070
nin2g <nin2g@yahoo.com>
2023-06-22 18:33:34 -0700

    multipy

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multiply two integer inputs
  * @a: the first integer input
  * @b: the second integer input
- * Return: should return the result of multiplying a by b. 
+ * Return: should return the result of multiplying a by b.
  */
 
 int mul(int a, int b)
