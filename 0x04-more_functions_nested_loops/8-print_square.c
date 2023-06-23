0161d8a088db12daa4c9d440451ba0d6998491d3
nin2g <nin2g@yahoo.com>
2023-06-23 13:28:19 -0700

    prntsqr

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_suare - prints a square followed by a new line
+ * print_square - prints a square followed by a new line
  * @s: the number of squares
  * Return: should return empty
  */
