dc5e881a97ba0e43973f0611247e6eed80b11c54
nin2g <nin2g@yahoo.com>
2023-06-23 13:25:44 -0700

    prntsqr

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_suare - prints a square followed by a new line
+ * @s: the number of squares
+ * Return: should return empty
+ */
+
+void print_square(int size)
+
+{
+
+	int x, y;
+
+	if (s <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < s; x++)
+	{
+	for (y = 0; y < s; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
