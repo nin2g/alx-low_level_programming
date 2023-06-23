e67dd5ceb90ee4a87a1c232d71dde92ebcf5e972
nin2g <nin2g@yahoo.com>
2023-06-23 15:08:43 -0700

    prnttria

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints squares in a tringular form  in the termainal
+ * @s: the size or the triangle of squares
+ * Return: should return nothing.
+ */
+
+void print_triangle(int s)
+
+{
+
+	int x, y, z;
+
+	if (s <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < s; x++)
+	{
+	for (y = s - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}	
