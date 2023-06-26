e06db64e2a5009e5a274171b22645923c460ea25
nin2g <nin2g@yahoo.com>
2023-06-26 12:57:34 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: the destination value
+ * @scr: the source value
+ * Return: the pointer to the dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
+}
