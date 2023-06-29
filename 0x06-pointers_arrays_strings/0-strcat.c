ec7478f5a12eb7b455a893550841a5521e71e810
nin2g <nin2g@yahoo.com>
2023-06-29 11:35:16 -0700

    concatn

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates two strings.
+ * @s: source string
+ * @d: destination string
+ * Return: should return a pointer to destination string.
+ */
+
+char *_strcat(char*d, char *s)
+
+{
+	int dlen = 0, i;
+
+	while (d[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		d[dlen] = s[i];
+		dlen++;
+	}
+
+	d[dlen] = '\0';
+	return (d);
+}
