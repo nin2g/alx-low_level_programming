616bf75a109e9089b855ff329aa6d62c17b81249
nin2g <nin2g@yahoo.com>
2023-06-29 17:03:15 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "'main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to the first string
+ * @s2 pointer to the second string
+ * Return:should return 0 if str1 == str2
+ * Othrewise should return difference of sutracting str1 from str1.
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	while (*s1 && *s2 && *S1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1  - *s2);
+}
