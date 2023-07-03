3c31b156b5e4bb6d25a7d04222963588c10246a8
nin2g <nin2g@yahoo.com>
2023-07-03 16:09:04 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
 /**
  * _strpbrk - searches for a string for any of a set of bytes
  * @s: the set of bytes to be searched.
- * @accept: the string to be searched. 
+ * @accept: the string to be searched.
  * Return: should return a pointer to the byte in s that matches one
  * of the bytes in accept, or NULL if no such byte is found
  */
@@ -17,9 +17,9 @@ char *_strpbrk(char *s, char *accept)
 		for (n = 0; accept[n] != '\0'; n++)
 		{
 			if (s[i] == accept[n])
-				return (s+i);
+				return (s + i);
 		}
-	
+
 	}
 	return (NULL);
 }
